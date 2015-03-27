bf3f895e18a92df6a272016e792ca7d85de32007
luzx <luzx@Ctrip.com>
2015-03-27 11:13:11 +0800

    add

---
+++ action.php
@@ -0,0 +1,8 @@
+<?php
+include_once "database.php";
+
+$_POST("")
+
+function decode($str){
+    if()
+}
---
+++ keys.php
@@ -0,0 +1,8 @@
+<?php
+$PRIVATE_KEYS = [
+    "GhGjLoUmN1B2B3V1H3R1U3I3",
+    "dsaddLOhkiHKhkHkhkhkdhsO",
+    "NIOUh890GH890bgKnhibgUI8",
+    "FTFyuV787799780GHUVGFfyy",
+    "N56789790GIBGIGFYDF78G78",
+]
